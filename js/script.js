1dff83683afae5d7c5ab18af048e8a8e1233af4d
Bonnie-Zhu <124389118+Bonnie-Zhu@users.noreply.github.com>
2023-04-18 17:22:59 +0000

    418

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Bonnie Zhu
+// Created on: April. 2023
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+* This function calculates your pay depending on the amount of hours you work (including tax).
+*/
+function calculate() {
+  // input
+  const Radius = parseInt(document.getElementById('Radius').value)
+
+  // process
+  const Volume = (4 / 3) * Math.PI * Radius ** 3
+
+  // output
+  document.getElementById('Volume').innerHTML = 'Volume is: ' + Volume + ' cm³'
+}
+
